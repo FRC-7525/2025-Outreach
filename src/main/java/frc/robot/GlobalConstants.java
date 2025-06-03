6017175c167cfb8c9d0751b497088fcb71badd58
Aitan Martinez <prisimrobotics@gmail.com>
2025-06-03 11:12:06 -0500

    Project Structure

---
+++ src/main/java/frc/robot/GlobalConstants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class GlobalConstants {
+    public enum RobotMode {
+        REAL,
+        TESTING,
+        SIM
+    } // No point in having replay tbh
+
+    public static final RobotMode ROBOT_MODE = RobotBase.isReal() ? RobotMode.REAL : RobotMode.SIM; // Change this to TESTING or SIM for testing purposes
+
+    public static final double SIMULATION_PERIOD = 0.02;
+}
