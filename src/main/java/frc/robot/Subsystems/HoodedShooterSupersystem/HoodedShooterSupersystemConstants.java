9b6bd775900a654dcd4acaa23142fb82dc9abc9e
Aitan Martinez <prisimrobotics@gmail.com>
2025-06-08 16:23:03 -0500

    HoodedShooterSupersystem

---
+++ src/main/java/frc/robot/Subsystems/HoodedShooterSupersystem/HoodedShooterSupersystemConstants.java
@@ -0,0 +1,5 @@
+package frc.robot.Subsystems.HoodedShooterSupersystem;
+
+public class HoodedShooterSupersystemConstants {
+    public static final String SUBSYSTEM_NAME = "HoodedShooterSupersystem";
+}
---
+++ src/main/java/frc/robot/Subsystems/HoodedShooterSupersystem/HoodedShooterSupersystemStates.java
@@ -0,0 +1,36 @@
+package frc.robot.Subsystems.HoodedShooterSupersystem;
+
+import org.team7525.subsystem.SubsystemStates;
+
+import frc.robot.Subsystems.AdjustableHood.AdjustableHoodStates;
+import frc.robot.Subsystems.Shooter.ShooterStates;
+
+public enum HoodedShooterSupersystemStates implements SubsystemStates {
+    IDLE("IDLE", AdjustableHoodStates.IDLE, ShooterStates.IDLE),
+    FIXED("FIXED", AdjustableHoodStates.FIXED, ShooterStates.IDLE),
+    DYNAMIC("DYNAMIC", AdjustableHoodStates.DYNAMIC, ShooterStates.DYNAMIC);
+
+    private final String stateString;
+    private final AdjustableHoodStates adjustableHoodState;
+    private final ShooterStates shooterState;
+
+    HoodedShooterSupersystemStates(String stateString, AdjustableHoodStates adjustableHoodState, ShooterStates shooterState) {
+        this.stateString = stateString;
+        this.adjustableHoodState = adjustableHoodState;
+        this.shooterState = shooterState;
+    }
+
+    @Override
+    public String getStateString() {
+        return stateString;
+    }
+
+    public AdjustableHoodStates getAdjustableHoodState() {
+        return adjustableHoodState;
+    }
+
+    public ShooterStates getShooterState() {
+        return shooterState;
+    }
+
+}
