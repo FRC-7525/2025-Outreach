013f24ef80d95d4e4307257d0680fac35e2d357b
Aitan Martinez <prisimrobotics@gmail.com>
2025-10-26 00:09:37 -0500

    Enhance HoodedShooterSupersystem with ShooterDataPoint class and interpolation logic

--- src/main/java/frc/robot/Subsystems/HoodedShooterSupersystem/HoodedShooterSupersystemConstants.java
+++ src/main/java/frc/robot/Subsystems/HoodedShooterSupersystem/HoodedShooterSupersystemConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.Subsystems.HoodedShooterSupersystem;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+import java.util.List;
 
 import edu.wpi.first.units.measure.AngularVelocity;
 
@@ -14,6 +17,10 @@ public class HoodedShooterSupersystemConstants {
 	public static final AngularVelocity MEDIUM_SPEED = RotationsPerSecond.of(1500);
 
 	// Random values and stuff
-	public static final double[] ANGLES = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
-	public static final double[] DISTANCES = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	public static final List<ShooterDataPoint> SHOOTER_DATA_POINTS = List.of(
+		new ShooterDataPoint(RotationsPerSecond.of(1500), Degrees.of(30), Meters.of(2.0)),
+		new ShooterDataPoint(RotationsPerSecond.of(2000), Degrees.of(35), Meters.of(3.0)),
+		new ShooterDataPoint(RotationsPerSecond.of(2500), Degrees.of(40), Meters.of(3.5)),
+		new ShooterDataPoint(RotationsPerSecond.of(3000), Degrees.of(45), Meters.of(4.0))
+	);
 }
